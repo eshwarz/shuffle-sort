0352b9df504df60063bdb294c985813915054258
Eshwar Chandra <mail@eshwar.me>
2020-11-07 01:17:30 +0530

    Desktop UI

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+/**
+ * Shuffle and sort class which tales care of the functionality
+ */
+class ShuffleSort {
+
+  /**
+   * Constructor
+   */
+  constructor() {
+    this.shuffle();
+    this.sort();
+  }
+
+  /**
+   * Shuffling functionality
+   */
+  shuffle = () => {
+    console.log('shuffle function!');
+  }
+
+  /**
+   * Sorting functionality
+   */
+  sort = () => {
+    console.log('sort function!');
+  }
+
+}
+
+// creating an instance of the shuffle and sort application
+new ShuffleSort();
